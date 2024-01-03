72e43792373e1f0eac1be53312961de528c3cf8a
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-01-03 23:18:42 +0530

    Spirtal Traversal

--- Recurssion/Palindrome.js
+++ Recurssion/Palindrome.js
@@ -1,92 +1,86 @@
-
 class ReadlineConsole {
-    //Class Constructor: 
-    //In JavaScript, a constructor is a special method for creating and initializing an object created with a class. 
-    //In ReadlineConsole class, the constructor() method sets up the initial state of an object.
-    constructor() {
-        this.numbers = [];//This line initializes an array property called numbers on the class instance. This array will be used to store the numbers that are input by the user.
-        //The this keyword refers to the current instance of the class, so this.numbers is a property specific to each instance of ReadlineConsole.
-        this.readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });//This line sets up a readline interface for input/output operations in the console.
+  constructor() {
+    this.numbers = [];
+    this.readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+
+  async getNumbers() {
+    const ask = async (question) => {
+      return new Promise((resolve) => {
+        this.readline.question(question, resolve);
+      });
+    };
+
+    let input = await ask(
+      "Enter required number of integers separated by spaces and then press enter: "
+    );
+    input = input.trim().replace(/\s+/g, " ");
+
+    let numbersArray = input.split(" ");
+
+    for (let i = 0; i < numbersArray.length; i++) {
+      let number = parseInt(numbersArray[i]);
+      if (!isNaN(number)) {
+        this.numbers.push(number);
+      }
     }
 
-    async getNumbers() {
-        const ask = async (question) => {
-            return new Promise(resolve => {
-                this.readline.question(question, resolve);
-            });
-        };
+    this.readline.close();
+  }
 
-        let input = await ask('Enter required number of integers separated by spaces and then press enter: ');
-        input = input.trim().replace(/\s+/g, ' '); // Remove leading/trailing spaces and consecutive spaces
-
-        let numbersArray = input.split(' ');
-
-        for (let i = 0; i < numbersArray.length; i++) {
-            let number = parseInt(numbersArray[i]);
-            if (!isNaN(number)) {
-                this.numbers.push(number);
-            }
-        }
-
-        this.readline.close();
+  async showNumbers() {
+    for (let i = 0; i < this.numbers.length; i++) {
+      console.log(this.numbers[i]);
     }
+  }
 
-    async showNumbers() {
-        for (let i = 0; i < this.numbers.length; i++) {
-            console.log(this.numbers[i]);
-        }
-    }
+  // palindrom(num){
+  //     console.log("num ===>",num)
+  //     if(num<0){
+  //         return false
+  //     }
+  //     let originalNum = num
+  //     let reversedNum = 0
+  //     while(num>0){
+  //         reversedNum = (reversedNum*10)  + (num%10)
+  //         console.log("reversedNum ===>",reversedNum)
+  //         num = Math.floor(num / 10);
+  //     }
+  //     return originalNum === reversedNum
+  // }
 
+  isPalindromeRecursive(num) {
+    if (num < 0) return false;
 
-   
-    // palindrom(num){
-    //     console.log("num ===>",num)
-    //     if(num<0){
-    //         return false
-    //     }
-    //     let originalNum = num
-    //     let reversedNum = 0
-    //     while(num>0){
-    //         reversedNum = (reversedNum*10)  + (num%10)    
-    //         console.log("reversedNum ===>",reversedNum)   
-    //         num = Math.floor(num / 10);
-    //     }
-    //     return originalNum === reversedNum
-    // }
+    const digitCount = Math.floor(Math.log10(num)) + 1;
+    return this.isPalindromeHelper(num, digitCount);
+  }
 
+  isPalindromeHelper(num, digitCount) {
+    if (num === 0) return true;
+    if (digitCount <= 1) return true;
 
+    const firstDigit = Math.floor(num / Math.pow(10, digitCount - 1));
+    const lastDigit = num % 10;
 
-    isPalindromeRecursive(num) {
-        if (num < 0) return false; // Negative numbers are not palindromes
+    if (firstDigit !== lastDigit) return false;
 
-        const digitCount = Math.floor(Math.log10(num)) + 1;
-        return this.isPalindromeHelper(num, digitCount);
-    }
+    num = Math.floor((num % Math.pow(10, digitCount - 1)) / 10);
+    digitCount -= 2;
 
-    isPalindromeHelper(num, digitCount) {
-        if (num === 0) return true;
-        if (digitCount <= 1) return true;
-
-        const firstDigit = Math.floor(num / Math.pow(10, digitCount - 1));
-        const lastDigit = num % 10;
-
-        if (firstDigit !== lastDigit) return false;
-
-        // Remove the first and last digit
-        num = Math.floor((num % Math.pow(10, digitCount - 1)) / 10);
-        digitCount -= 2;
-
-        return this.isPalindromeHelper(num, digitCount);
-    }
+    return this.isPalindromeHelper(num, digitCount);
+  }
 }
 
 (async () => {
-    const readConsole = new ReadlineConsole();
-    await readConsole.getNumbers();
-    //await readConsole.showNumbers();
-    const isPalindrome = readConsole.isPalindromeRecursive(readConsole.numbers[0]);
-    console.log('Is palindrome:', isPalindrome);
+  const readConsole = new ReadlineConsole();
+  await readConsole.getNumbers();
+  //await readConsole.showNumbers();
+  const isPalindrome = readConsole.isPalindromeRecursive(
+    readConsole.numbers[0]
+  );
+  console.log("Is palindrome:", isPalindrome);
 })();
--- Sort/QuickSort.js
+++ Sort/QuickSort.js
@@ -1,76 +1,72 @@
-
 class ReadlineConsole {
-    constructor() {
-        this.numbers = [];
-        this.readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+  constructor() {
+    this.numbers = [];
+    this.readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+
+  async getNumbers() {
+    const ask = async (question) => {
+      return new Promise((resolve) => {
+        this.readline.question(question, resolve);
+      });
+    };
+
+    let input = await ask(
+      "Enter required number of integers separated by spaces and then press enter: "
+    );
+    input = input.trim().replace(/\s+/g, " ");
+
+    let numbersArray = input.split(" ");
+
+    for (let i = 0; i < numbersArray.length; i++) {
+      let number = parseInt(numbersArray[i]);
+      if (!isNaN(number)) {
+        this.numbers.push(number);
+      }
     }
 
-    async getNumbers() {
-        const ask = async (question) => {
-            return new Promise(resolve => {
-                this.readline.question(question, resolve);
-            });
-        };
+    this.readline.close();
+  }
 
-        let input = await ask('Enter required number of integers separated by spaces and then press enter: ');
-        input = input.trim().replace(/\s+/g, ' '); // Remove leading/trailing spaces and consecutive spaces
+  async showNumbers() {
+    for (let i = 0; i < this.numbers.length; i++) {
+      console.log(this.numbers[i]);
+    }
+  }
 
-        let numbersArray = input.split(' ');
-
-        for (let i = 0; i < numbersArray.length; i++) {
-            let number = parseInt(numbersArray[i]);
-            if (!isNaN(number)) {
-                this.numbers.push(number);
-            }
-        }
-
-        this.readline.close();
+  //Step 1 - Pick a pivot point any element
+  //Step 2 - Take that element and place it in the correct place in the sorted arrray
+  //Step 3 - Put everything that's smaller than the pivot into a 'left' array and everything that's greater than the pivot into a 'right' array
+  //Step 4 - Repeat the process for the individual ‘left' and 'right' arrays till you have an array of length 1 which is sorted by definition
+  //Step 5 - Repeatedly concatenate the left array, pivot and right array till one sorted array remains
+  quickSort(arr) {
+    if (arr.length < 2) {
+      return arr;
     }
 
-    async showNumbers() {
-        for (let i = 0; i < this.numbers.length; i++) {
-            console.log(this.numbers[i]);
-        }
+    let pivot = arr[arr.length - 1];
+
+    let left = [];
+    let right = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
     }
 
-
-    //Step 1 - Pick a pivot point any element
-    //Step 2 - Take that element and place it in the correct place in the sorted arrray
-    //Step 3 - Put everything that's smaller than the pivot into a 'left' array and everything that's greater than the pivot into a 'right' array
-    //Step 4 - Repeat the process for the individual ‘left' and 'right' arrays till you have an array of length 1 which is sorted by definition
-    //Step 5 - Repeatedly concatenate the left array, pivot and right array till one sorted array remains
-    quickSort(arr) { // This is the name of our sorting game, and 'arr' is our list of numbers (like our toy cars).
-       
-        if(arr.length < 2){ // If our list has 0 or 1 number, it's already sorted (because a single toy car doesn't need sorting).
-            return arr // So we just return it as is.
-        }
-    
-        let pivot = arr[arr.length - 1]; // We pick the last number in the list as our "special number" (pivot).
-    
-        let left = [] // We make a new, empty list called 'left' for numbers smaller than our special number.
-        let right = [] // We also make another new, empty list called 'right' for numbers bigger than our special number.
-        for(let i = 0; i < arr.length - 1; i++){ // Now we start a loop to look at each number in our list (except the special number).
-            if(arr[i] < pivot){ // If the current number is smaller than the special number,
-                left.push(arr[i]) // We put it in the 'left' list.
-            } else { // If it's not smaller (which means it's equal to or bigger),
-                right.push(arr[i]) // We put it in the 'right' list.
-            }
-        }
-        // Now we play the sorting game with the 'left' list and the 'right' list,
-        // then put the sorted 'left' list, our special number, and the sorted 'right' list together.
-        return [...this.quickSort(left), pivot, ...this.quickSort(right)] 
-    }
-    
-    
+    return [...this.quickSort(left), pivot, ...this.quickSort(right)];
+  }
 }
 
 (async () => {
-    const readConsole = new ReadlineConsole();
-    await readConsole.getNumbers();
-    readConsole.showNumbers();
-    const product = readConsole.quickSort(readConsole.numbers)
-    console.log(product)
+  const readConsole = new ReadlineConsole();
+  await readConsole.getNumbers();
+  readConsole.showNumbers();
+  const product = readConsole.quickSort(readConsole.numbers);
+  console.log(product);
 })();
