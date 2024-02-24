a4982fb116de911963ac7f11bc6592f524caa413
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-02-24 21:41:40 +0530

    dllfeb24

---
+++ Linked Lists/DoublyLLReverse.js
@@ -0,0 +1,31 @@
+class Node{
+    constructor(x){
+        this.data = x;
+        this.prev = null;
+        this.next = null;
+      }
+}
+
+
+class Solution{
+    reverseDLL(head){
+        reverseDLL(head){
+            let current = head;
+             let temp = null;
+             while (current != null) {
+                  
+                   temp = current.prev;
+                   current.prev = current.next;
+                   current.next = temp;
+       
+                   current = current.prev;
+               }
+       
+               if (temp != null) {
+                   head = temp.prev;
+               }
+       
+               return head; 
+         }
+    }
+}
