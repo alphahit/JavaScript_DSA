9d555bd44f8fc562a65cde00c76cdef61f1e3edf
prateekpriyadarshi <prateek.priyadarshi@squbix.com>
2024-03-15 18:17:40 +0530

    Mar 15

--- Arrays/MinimumSwaps.js
+++ Arrays/MinimumSwaps.js
@@ -1,12 +1,32 @@
 let arr =[2, 1, 5, 6, 3]
-let k = 6 
+let k = 3
 function minSwap(arr,k){
-    let favourable;
-    let nonFavourable;
+    let favourable = 0;
+    let nonFavourable = 0;
     for(let i = 0; i<arr.length; i++){
-        if(arr[i]<k){
+        if(arr[i]<=k){
             favourable++
         }
     }
-    for()
+    for(let j = 0; j<favourable; j++){
+        if(arr[j]>k){
+            nonFavourable++
+        }
+    }
+   
+    let swap = nonFavourable
+
+    for(let i = 0, j = favourable; j < arr.length; ++i, ++j){
+        if(arr[i] > k){
+            --nonFavourable
+        }
+        if(arr[j] > k){
+            ++nonFavourable
+        }
+        swap = Math.min(nonFavourable,swap);
+    }
+    return swap;
+    
 }
+
+console.log(minSwap(arr,k))
