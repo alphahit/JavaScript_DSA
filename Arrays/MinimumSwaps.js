2d012e4a7cff22d75cd50e09723e829955a40346
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-03-15 08:49:33 +0530

    march 15

---
+++ Arrays/MinimumSwaps.js
@@ -0,0 +1,12 @@
+let arr =[2, 1, 5, 6, 3]
+let k = 6 
+function minSwap(arr,k){
+    let favourable;
+    let nonFavourable;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]<k){
+            favourable++
+        }
+    }
+    for()
+}
