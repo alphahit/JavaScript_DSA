b3ed9ac17dbb35789ff4203def101d3fbf8a89e5
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-03-22 22:40:13 +0530

    march 22

--- Binary Tree/SumLongestPath.js
+++ Binary Tree/SumLongestPath.js
@@ -1,10 +1,9 @@
-class Node{
-    constructor(val){
-        this.data = val;
-        this.left = null;
-        this.right = null;
-    }
-    
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 const one = new Node(1);
@@ -24,6 +23,25 @@ five.right = seven;
 five.left = six;
 seven.left = eight;
 
-function maxHeightSum(node, sum){
-    
+function maxHeightSum(root) {
+  let maxSum = -Infinity;
+  let maxLength = 0;
+  if (!root) return 0;
+  function sumLongestPath(root, sum, len) {
+    if (!root) {
+      if (maxLength < len) {
+        maxLength = len;
+        maxSum = sum;
+      } else if (maxLength === len && maxSum < sum) {
+        maxSum = sum;
+      }
+      return;
+    }
+    sumLongestPath(root.left, sum + root.data, len + 1);
+    sumLongestPath(root.right, sum + root.data, len + 1);
+  }
+  sumLongestPath(root, 0, 0);
+  return maxSum;
 }
+
+console.log(maxHeightSum(one));
