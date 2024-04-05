1fd787b32096b3a29f550bdefaa117265f6f30dd
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-04-05 23:09:18 +0530

    april 5

--- Linked Lists/QuickSort.js
+++ Linked Lists/QuickSort.js
@@ -1,99 +1,110 @@
 class Node {
-    constructor(data){
-        this.data = data;
-        this.next = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor(data){
-        this.head = null;
-    }
-    addFirst(data){
-        const newNode = new Node(data);
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-    size(){
-        let count = 0;
-        if(!this.head) return count
+  constructor(data) {
+    this.head = null;
+  }
+  addFirst(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+  size() {
+    let count = 0;
+    if (!this.head) return count;
 
-        let current = this.head
-        while(current){
-            count ++
-            current = current.next
-        }
-        return count
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
     }
-    print(){
-        let current = this.head;
-        while(current){
-            console.log(current.data)
-            current = current.next
-        }
+    return count;
+  }
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
     }
+  }
 }
 
 class QuickSortLinkedList extends LinkedList {
+  sort() {
+    this.head = this.quickSort(this.head, this.getPivot(this.head));
+  }
 
-    sort(){
-        this.head = this.quickSort(this.head, this.getPivot(this.head));
+  getPivot(node) {
+    while (node && node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  partition(head, end) {
+    let pivot = end.data;
+    let prev = null,
+      tail = end,
+      current = head;
+
+    while (current !== end && current != null) {
+      if (current.data < pivot) {
+        if (prev === null) {
+          head = current;
+        } else {
+          prev.next = current;
+        }
+        prev = current;
+      } else {
+        tail.next = current;
+        tail = current;
+      }
+      current = current.next;
+    }
+    tail.next = null;
+    if (prev !== null) {
+      prev.next = end;
+    }
+    return head;
+  }
+
+  quickSort(start, end) {
+    if (start == null || start === end) {
+      return start;
     }
 
-    getPivot(node){
-        while(node && node.next){
-            node = node.next;
-        }
-        return node;
+    let partitioned = this.partition(start, end),
+      pivot = start;
+
+    while (pivot != null && pivot.next != null && pivot.next != end) {
+      pivot = pivot.next;
     }
 
-    partition(head, end){
-        let pivot = end.data;
-        let prev = null, tail = end, current = head;
-
-        while(current !== end){
-            if(current.data < pivot){
-                if(prev === null){
-                    head = current;
-                }else{
-                    prev.next = current;
-                }
-                prev = current;
-            }else{
-                tail.next = current;
-                tail = current;
-            }
-            current = current.next;
-        }
-        tail.next = null; // very important to avoid cycles in linked list
-        if(prev !== null){
-            prev.next = end;
-        }
-        return head;
+    if (pivot != null && pivot.next == end) {
+      pivot.next = null;
     }
 
-    quickSort(start, end){
-        if(start == null || start === end){
-            return start;
-        }
+    let newStart = null;
+    if (partitioned != end) {
+      newStart = this.quickSort(partitioned, pivot);
 
-        let newStart = null, newEnd = null;
-
-        newStart = this.partition(start, end);
-        if(newStart !== end){
-            start = this.quickSort(newStart, end);
-            newEnd = end.next;
-            end.next = newEnd;
-        }
-
-        if(newEnd != null){
-            this.quickSort(end.next, newEnd);
-        }
-
-        return newStart;
+      if (pivot != null) {
+        pivot.next = end;
+      }
     }
+
+    if (end != null && end.next != null) {
+      end.next = this.quickSort(end.next, this.getPivot(end.next));
+    }
+
+    return newStart == null ? start : newStart;
+  }
 }
-
 
 const sortedLinkedList = new QuickSortLinkedList();
 
@@ -102,6 +113,6 @@ sortedLinkedList.addFirst(13);
 sortedLinkedList.addFirst(8);
 sortedLinkedList.addFirst(5);
 sortedLinkedList.addFirst(2);
-sortedLinkedList.print(); 
-sortedLinkedList.sort(); 
-sortedLinkedList.print(); 
+
+sortedLinkedList.sort();
+sortedLinkedList.print();
