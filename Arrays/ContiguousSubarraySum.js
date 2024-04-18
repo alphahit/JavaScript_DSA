acb0941e9465e920d05f55bfdf77e97b21106f80
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-04-18 08:18:29 +0530

    apr18

---
+++ Arrays/ContiguousSubarraySum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+     const prefixSumMod = new Map();
+     let prefixSum = 0;
+     prefixSumMod.set(0,-1);
+     for (let i = 0; i < nums.length; i++){
+        prefixSum +=  nums[i];
+        let mod = prefixSum % k;
+
+        if(mod < 0) mod += k;
+
+        if(prefixSumMod.has(mod)){
+            if(i - prefixSumMod.get(mod) > 1   ){
+                return true;
+            }
+        }else{
+            prefixSumMod.set(mod,i);
+        }
+     }
+     return false;
+};
