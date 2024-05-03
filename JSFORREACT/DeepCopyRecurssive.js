527bcadc929a99e4ec0ee0203b563cf1302dc46e
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-05-03 23:20:01 +0530

    may 3

--- JSFORREACT/DeepCopyRecurssive.js
+++ JSFORREACT/DeepCopyRecurssive.js
@@ -13,7 +13,7 @@ const company = {
                     employees: [
                         { name: "Charlie", role: "QA Engineer" }
                     ],
-                    subdepartments: []  // No further nesting here
+                    subdepartments: []
                 }
             ]
         },
@@ -23,21 +23,55 @@ const company = {
                 { name: "David", role: "Recruiter" },
                 { name: "Eve", role: "Benefits Coordinator" }
             ],
-            subdepartments: []  // No further nesting here
+            subdepartments: []
         }
     ]
 };
 
 
-function deepClone(obj) {
-    for(let i = 0; i < obj.keys().length; i++){
-        if(typeof obj[obj.keys()[i]] === 'object'){
-            obj[obj.keys()[i]] = deepClone(obj[obj.keys()[i]]);}
-            else{
-                obj[obj.keys()[i]] = obj[obj.keys()[i]];
-            }
+function deepClone(obj, seen = new Map()) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
     }
+
+    if (Array.isArray(obj)) {
+        const newArray = [];
+        seen.set(obj, newArray);
+        for (const item of obj) {
+            newArray.push(deepClone(item, seen));
+        }
+        return newArray;
+    }
+
+    if (obj instanceof Object) {
+        if (seen.has(obj)) {
+            return seen.get(obj);
+        }
+
+        const newObj = {};
+        seen.set(obj, newObj);
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                newObj[key] = deepClone(obj[key], seen);
+            }
+        }
+        return newObj;
+    }
+
+    return obj;
 }
+
+
+
+// Add recursive reference
+company.departments.push(company.departments[0]);
+
+const clonedCompany = deepClone(company);
+
+// Verify the deep clone
+console.log(clonedCompany);
+console.log(clonedCompany.departments[0] === clonedCompany.departments[1]); // should be true
+console.log(clonedCompany.departments[0] === company.departments[0]); // should be false
 
 
 
