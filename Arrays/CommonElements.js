e834c25d3fbf88d69a024b8716a37e97dea663be
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2025-06-10 00:41:10 +0530

    Common Elements Intersection 3 arrays Solve Without HAshMap

--- Arrays/CommonElements.js
+++ Arrays/CommonElements.js
@@ -21,4 +21,46 @@ function checkIntersection(A,B,C) {
     return elements
 }
 
-console.log(checkIntersection(A,B,C))
+// console.log(checkIntersection(A,B,C))
+
+
+function commonWithoutHashMap(A,B,C){
+    let fp = 0
+    let sp = 0
+    let tp = 0
+
+//    let minLengthArr = 0
+//     if(A.length > B.length){
+//         if(B.length > C.length ){
+//             minLengthArr = C.length
+//         }else{
+//             minLengthArr = B.length
+//         }
+//     }else if(A.length > C.length){
+//          minLengthArr = C.length
+//     }else{
+//          minLengthArr = A.length
+//     }
+
+  let output = []
+    while(fp < A.length && sp< B.length && tp < C.length){
+        if((A[fp] === B[sp])  && (B[sp]  === C[tp])){
+            output.push(A[fp])
+            fp += 1
+            sp += 1
+            tp += 1
+        }else if(A[fp] < B[sp] && A[fp] < C[tp]){
+            fp += 1
+        }else if( B[sp] < A[fp] && B[sp] < C[tp]){
+            sp += 1
+        }else if(C[tp] < A[fp] && C[tp] < B[sp]){
+            tp+= 1
+        }
+       
+    }
+    return output.length === 0 ?  [-1] : output
+
+}
+
+
+console.log(commonWithoutHashMap(A,B,C))
